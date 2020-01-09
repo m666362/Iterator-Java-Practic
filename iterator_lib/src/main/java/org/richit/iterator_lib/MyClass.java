00e013c51c61a485afdecc183e41d198754df81a
Md Rayhan <mdrayhan9464@gmail.com>
2020-01-09 12:11:37 +0600

    first comment

---
+++ iterator_lib/src/main/java/org/richit/iterator_lib/MyClass.java
@@ -0,0 +1,16 @@
+package org.richit.iterator_lib;
+
+public class MyClass {
+
+    static String dot = "----------------";
+    private static void heading(String heading) {
+        System.out.println();
+        System.out.println(dot + heading + dot);
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        heading("Programme Started");
+    }
+
+}
